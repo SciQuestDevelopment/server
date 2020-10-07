9f76bbd826a943d78c35a547c425df6b1e590e61
aooXu <yichao.xu.19@ucl.ac.uk>
2020-10-07 20:55:12 +0800

    router and db initialise

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from typing import Any
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world() -> str:
-    return str({
+def hello_world():
+    return jsonify({
         "Get article details": "GET /post/:post_id",
         "Get all article details": "GET /post",
         "Get all article details of a user": "GET /posts/author/:userID",
@@ -16,32 +18,32 @@ def hello_world() -> str:
 
 
 @app.route('/post/<post_id>', methods=['GET'])
-def get_post(post_id: str) -> str:
+def get_post(post_id: str):
     return f'Get article details of {post_id}'
 
 
 @app.route('/post', methods=['GET'])
-def get_all_post() -> str:
+def get_all_post():
     return 'Get all article details of a user'
 
 
 @app.route('/posts/author/<user_id>', methods=['GET'])
-def get_all_post_of_author(user_id: str) -> str:
+def get_all_post_of_author(user_id: str):
     return f'Get all article details of a user {user_id}'
 
 
 @app.route('/post', methods=['PUT'])
-def create_post() -> str:
+def create_post():
     return 'Creat a article'
 
 
 @app.route('/post/<post_id>', methods=['DELETE'])
-def delete_post(post_id: str) -> str:
+def delete_post(post_id: str):
     return f'Delete a article {post_id}'
 
 
 @app.route('/post/<post_id>', methods=['POST'])
-def update_post(post_id: str) -> str:
+def update_post(post_id: str):
     return f'Modify details of a article {post_id}'
 
 
