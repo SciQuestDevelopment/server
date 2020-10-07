b57bd8ba3ee5cd06d2f8d865cf38e9eac9a32871
aooXu <yichao.xu.19@ucl.ac.uk>
2020-10-07 17:52:47 +0800

    Route

--- app.py
+++ app.py
@@ -4,8 +4,45 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello_world() -> str:
+    return str({
+        "Get article details": "GET /post/:post_id",
+        "Get all article details": "GET /post",
+        "Get all article details of a user": "GET /posts/author/:userID",
+        "Creat a article": "PUT /post",
+        "Delete a article": "DELETE /posts/:postID",
+        "Modify details of a article": "POST /posts/:postID"
+    })
+
+
+@app.route('/post/<post_id>', methods=['GET'])
+def get_post(post_id: str) -> str:
+    return f'Get article details of {post_id}'
+
+
+@app.route('/post', methods=['GET'])
+def get_all_post() -> str:
+    return 'Get all article details of a user'
+
+
+@app.route('/posts/author/<user_id>', methods=['GET'])
+def get_all_post_of_author(user_id: str) -> str:
+    return f'Get all article details of a user {user_id}'
+
+
+@app.route('/post', methods=['PUT'])
+def create_post() -> str:
+    return 'Creat a article'
+
+
+@app.route('/post/<post_id>', methods=['DELETE'])
+def delete_post(post_id: str) -> str:
+    return f'Delete a article {post_id}'
+
+
+@app.route('/post/<post_id>', methods=['POST'])
+def update_post(post_id: str) -> str:
+    return f'Modify details of a article {post_id}'
 
 
 if __name__ == '__main__':
