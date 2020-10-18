32cf643cf6431005d27aa8ec86665873b3f17f5b
dennis9707 <he0707@hotmail.com>
2020-10-18 13:40:14 +0800

    database test

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, request, json
-
+from flask_mysqldb import MySQL
+import yaml
 app = Flask(__name__)
 
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_PORT'] = db['mysql_port']
+app.config['MYSQL_USER '] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
 
 @app.route('/')
 def hello_world():
