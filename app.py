9b9b39374a7801b802e4901fb076de7175ba27cd
dennis9707 <he0707@hotmail.com>
2020-10-19 15:39:22 +0800

    initiate label relations script

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask_mysqldb import MySQL
 import yaml
 app = Flask(__name__)
 
-db = yaml.load(open('db.yaml'))
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_PORT'] = db['mysql_port']
-app.config['MYSQL_USER '] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
+# db = yaml.load(open('db.yaml'))
+# app.config['MYSQL_HOST'] = db['mysql_host']
+# app.config['MYSQL_PORT'] = db['mysql_port']
+# app.config['MYSQL_USER '] = db['mysql_user']
+# app.config['MYSQL_PASSWORD'] = db['mysql_password']
+# app.config['MYSQL_DB'] = db['mysql_db']
 
 @app.route('/')
 def hello_world():
