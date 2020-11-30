2811ff230beb42095a84fdfc509e37d99393a1d6
jialu gan <ucabjg3@ucl.ac.uk>
2020-11-30 17:10:32 +0800

    add api for finding the universities in one country

---
+++ api/utils/datasource/domainAPI.py
@@ -0,0 +1,35 @@
+import json
+from pprint import pprint
+
+
+# class for searching universities in diff countries
+# use query($country) to find the result
+class domainAPI:
+    def __init__(self):
+        # Load the source
+        self.src = None
+        with open('./world_universities_and_domains.json') as src_file:
+            self.src = json.load(src_file)
+
+    # it returns a list including names of all universities in that country
+    def query(self, country):
+        if self.src is None:
+            return []
+
+        def filter(entry, item):
+            matching = entry['country']
+
+            if item == matching or \
+                    item == matching.lower() or \
+                    item == matching.upper():
+                return True
+
+            else:
+                return False
+
+        return [entry['name'] for entry in self.src if filter(entry, country)]
+
+
+if __name__ == '__main__':
+    d = domainAPI()
+    pprint(d.query("china"))
