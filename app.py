5853f4683a7ba5d65a49181f3195fe2a657e97aa
aooXu <yichao.xu.19@ucl.ac.uk>
2020-12-12 20:28:31 +0800

    modify the security key to avoi the works issues

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ if os.path.exists(dotenv_path): load_dotenv(dotenv_path, override=True)
 from api import auth, post, scopus
 app = Flask(__name__)
 # 24bits random secret key
-app.config['SECRET_KEY'] = f'{os.urandom(24)}'
+secret_key = 'test' # os.urandom(24)
+app.config['SECRET_KEY'] = f'{secret_key}'
 app.permanent_session_lifetime = datetime.timedelta(minutes=30)
 # CORS to allow the cross-domain issues
 CORS(app, supports_credentials=True)
